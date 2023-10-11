cdb8a963bf18ab665678483449a6fd91fef0403a
YesCoach <sbsii1020@gmail.com>
2023-10-11 15:24:10 +0900

    Initial Commit

---
+++ CommuteFriend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommuteFriend
+//
+//  Created by 박태현 on 2023/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
